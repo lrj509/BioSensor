f0a0aa804e0c251097c5a70459eb20cb8705df7d
Amy Stewart <amystewart@nas10-241-17-149.york.ac.uk>
2018-01-24 13:00:46 +0000

    Sesnor and Camera

--- camera.py
+++ camera.py
@@ -16,18 +16,34 @@ class camera():
     
     def get_picture(self):
         
-        """
+        import time
+        import picamera
+
+        with picamera.PiCamera() as camera:
+
+            camera.start_preview()
+            time.sleep(0) 
+            camera.capture('/home/pi/Desktop/image.jpg') #Saves image to the desktop, can change to another location
+            camera.stop_preview()
         
-        """
-        
-        return
+        return 
     
-    def get_interprited_picture(self):
+    def get_interpreted_picture(self):
         
-        """
-        
-        """
-        
+       import Image #Might need to install Python image library
+       myImage=Image.open('image name')
+       myImage.show() #to display image 
+
+        black = makeColor (0,0,0)
+        red= makeColor(255,0,0) #Change colour for colour of luminescence 
+
+    #To calculate number of pixels of a given colour, black should be background of the image
+       for pixel in getPixels(picture):
+           color = getColor(pixel)
+           if color == black: numblacks += 1
+           else color == red: numreds += 1
+           print('black =' + str(black) + 'red =' str(red))
+
         return 
         
     
--- sensor.py
+++ sensor.py
@@ -14,34 +14,78 @@ class sensor():
         
         return
     
-    def get_sensor_1_value(self):
+   def get_sensor_1_value(self): #Analogue sensor
+
+        #Import python modules
+           import RPi.GPIO as GPIO
+           import time
+           
+        # Set variables
+           sensor_1_pin=
+           
+        # Pi setup
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(gpio_pin, GPIO.OUT)
+
+        def rc_time(gpio_pin):
+
+            # output on the pin to discharge capacitor
+            GPIO.setup(gpio_pin, GPIO.OUT)
+            GPIO.output(gpio_pin, GPIO.LOW)
+            time.sleep(0.05)
+
+            # reset as input
+            GPIO.setup(gpio_pin, GPIO.IN)
+
+            # count loops until voltage across capacitor reads high
+            timenow = time.time()
+            while (GPIO.input(gpio_pin) == GPIO.LOW):
+                pass
+    
+            return time.time() - timenow
+     
+
+            # catch exceptions and a clean exit
+        try:
+            # print light levels every 5 seconds for one minute
+            t = 0
+            while t <= 60:       
+                if t % 5 == 0: # if t modulus 5 is zero
+                    # check sensor
+                    rc = rc_time(sensor_1_pin)
+                    print(t, rc)
         
-        """
-        Returns the value for sensor one.
-        """
+            t += 1 # increment loop
+            time.sleep(0.95 - rc) # wait 1 second
+
+        except:
+            print('An error or exception occurred')
+
+        finally:
+            GPIO.cleanup()
         
         sensor_1_value = 12
-        
-        return(sensor_1_value)
     
-    def get_sensor_2_value(self):
-        
-        """
-        Returns the value for sensor two.
-        """
-        
-        sensor_2_value = 12
+    def get_sensor_2_value(self): #Digital sensor
+
+        #Import python modules
+            import time
+            import tsl2591
+            
+
+        tsl = tsl2591.Tsl2591()  # initialize
+        full, ir = tsl.get_full_luminosity()  # read raw values (full spectrum and ir spectrum)
+        lux = tsl.calculate_lux(full, ir)  # convert raw values to lux
+
+        return time.time() - timenow
+
+        try:
+            # print light levels every 5 seconds for one minute
+            t = 0
+            while t <= 60:       
+                if t % 5 == 0: # if t modulus 5 is zero
+                    print(t, lux)
         
         return(sensor_2_value)
-    
-    def get_sensor_3_value(self):
-        
-        """
-        Returns the value for sensor three.
-        """
-        
-        sensor_3_value = 12
-        
-        return(sensor_3_value)
     
     
