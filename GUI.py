22892413ed1c082bfcf37dc91ae926c1fb80132a
awb519 <Adam>
2018-02-04 14:23:50 +0000

    work now

--- GUI.py
+++ GUI.py
@@ -1,6 +1,9 @@
-from tkinter import * 
+from tkinter import *
+from tkinter import messagebox
+from tkinter import simpledialog
 from control import control
 
+y = 11
 
 x = control()
 print(x.self_check())
@@ -9,8 +12,8 @@ class GUI():
     
     """
     This class handles all the frontend functionality of the program.
-    
-    @author lrj509
+
+    @authors awb519 & lwj
     """
     
     def __init__(self,master):
@@ -19,18 +22,37 @@ class GUI():
         the init method for the class
         """
         frame = Frame(master)
-        frame.pack()
+        frame.grid()
 
+        self.variable = StringVar(master)
+        self.variable.set("Please Select") # default value
+
+##        text = Text(frame)
+##        text.insert(INSERT, "Hello....142.")
+##        text.grid(row = 1, column=2)
+
+        w = OptionMenu(frame, self.variable, "Opiates", "Cocaine")
+        w.grid(row =1, column =1, padx= 80, pady=20)
+
+        
         self.button = Button(
-            frame, text="QUIT", fg="red", command=frame.quit
-            )
-        self.button.pack(side=LEFT)
+            frame, text="Run Test", fg="Black", command=self.result)
+        self.button.grid(row =2, column =1, padx= 30,pady=5)
 
-        self.hi_there = Button(frame, text="Hello", command=self.self_test())
-        self.hi_there.pack(side=LEFT)
-        
-        
-        
+
+
+
+    def result(self):
+
+        text.grid(row = 1, column=2)
+        if y > 8:
+            messagebox.showinfo("Positive", self.variable.get() + " presence confirmed", command=self.self_test())
+        else:
+            messagebox.showinfo("Negative", "Substance not detected", command=self.self_test())
+
+
+ 
+
     def self_test(self):
         
         """
@@ -40,13 +62,14 @@ class GUI():
         
         result = x.self_check()
         
-        #Error checking 
+       #Error checking 
         
         if type(result) != bool:
             raise Exception("Self check should return a bool")
             
         else:
             print(result)
+
             
     
     
